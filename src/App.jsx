335b4a6943f65fc918aa6ccbb666f41e928061ea
Maylis <maylis.ameto@gmail.com>
2023-07-13 11:12:37 +0200

    (Add) Architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import loader from "./assets/loader.svg"
+
 function App() {
  
   return (
-
+      <>
       <main>
-      
+        <div className="loader-container">
+          <img src={loader} alt="loading icon" />
+        </div>
+        <p className="city-name">Miami</p>
+        <p className="country-name">USA</p>
+        <p className="temperature">27°C</p>
+        <div className="info-icon-container">
+          <img src="../public/icons/02d.svg" alt="weather icon" />
+        </div>
       </main>
+      </>
   
   );
 }
